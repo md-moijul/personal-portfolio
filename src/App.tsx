0a70647b6e99d1b9fce381e019a2c60e950f3e2b
moijul <jkmoijul@gmail.com>
2024-10-01 23:59:13 +0100

    dependency: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
-  return (
-    <>
-      hello wrold
-    </>
-  )
+    return (
+        <main className='min-h-screen bg-red-500 min-w-screen'>
+            <Button variant='outline'>hello world</Button>
+        </main>
+    )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    }
 })
