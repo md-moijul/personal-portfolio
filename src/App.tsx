fbf0997515bdb0f39ee19b2f8533937fb1274038
moijul1 <moijul@wilxite.com>
2024-10-04 23:58:02 +0100

    ui: responsive container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
 
 function App() {
 
     return (
-        <>
-            <h1 className="text-3xl font-bold underline bg-red-600">
-                Hello world!
-            </h1>
-        </>
+        <main className='min-h-screen min-w-screen bg-cyan-100'>
+            <section className='px-8 sx:px-12 sm:px-16 md:px-24 max-w-[1400px] mx-auto continer'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+            </section>
+        </main>
     )
 }
 
